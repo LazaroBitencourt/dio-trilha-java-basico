8fb012ba0edf2222da697fa3ecbf0780d6487888
Lázaro <lazaro_boc@yahoo.com.br>
2023-09-09 18:55:08 -0300

    Implementação do projeto refente ao desafio Controle de fluxo

---
+++ Desafio-Controle-Fluxo/src/ParametrosInvalidosException.java
@@ -0,0 +1,12 @@
+public class ParametrosInvalidosException extends Exception{
+
+    public ParametrosInvalidosException(){
+
+    }
+    public ParametrosInvalidosException(String msg){
+        super(msg);
+    }
+    public ParametrosInvalidosException(String msg, Throwable cause){
+        super(msg, cause);
+    }
+}
